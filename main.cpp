4c1dab7a467006fe58d4198172ff3260f81793f9
Drenajnayavoda <91183133+Drenajnayavoda@users.noreply.github.com>
2021-12-22 17:26:36 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
     const char *symbl = "0123456789abcdefghxyzABCDEFGHXYZ";
     /// длинна строки наверное вычислять должна 1 раз ? 
     /// не соответствует требованиям задания пароли
-    const size_t lenght = strlen(s);
+    const size_t lenght = strlen(symbl);
     for (int i = 0; i < lenght; i++)
         for (int j = 0; j < lenght; j++)
             for (int k = 0; k < lenght; k++)
